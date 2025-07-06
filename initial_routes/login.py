bac4f8272b590cfe9b678da9b5464f4a32c3af1c
adespinosa14 <adespinosa2002@gmail.com>
2025-07-05 17:18:22 -0700

    Created Login + Signup backend

--- initial_routes/login.py
+++ initial_routes/login.py
@@ -1,6 +1,12 @@
-from flask import app, render_template
+from flask import app, render_template, request, redirect, jsonify
 
 def register_login(app):
     @app.route('/login')
     def Login():
         return render_template('Initial_Home/login.html')
+
+    @app.route('/login/authenticate_user', methods=['GET', 'POST'])
+    def Authenticate_User():
+        if request.method == 'POST':
+            print(request.form)
+        return "<p> Login Successful </p>"
--- initial_routes/signup.py
+++ initial_routes/signup.py
@@ -1,6 +1,19 @@
-from flask import app, render_template
+from flask import Flask, app, render_template, request, redirect, jsonify, url_for
 
 def register_signup(app):
     @app.route('/signup')
     def Signup():
         return render_template('Initial_Home/signup.html')
+
+    @app.route('/signup/create_user', methods=['GET', 'POST'])
+    def Create_User():
+        if request.method == 'POST':
+            email_address = request.form['user_email']
+            password = request.form['user_password']
+            reenter_password = request.form['user_reenter_password']
+
+            print(email_address)
+            print(password)
+            print(reenter_password)
+        
+        return "<p> User Created Successfully </p>"

9c47874caa3a7854b60f066bd00e9fc447d36414
Andrew <adespinosa2002@gmail.com>
2026-01-23 16:08:08 -0800

    Created Fulle File Structure

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from backend.routes import register_routes
 
 base_directory = os.path.abspath(os.path.dirname(__file__))
 template_directory = os.path.join(base_directory, "frontend", "templates")
+static_directory = os.path.join(base_directory, "frontend", "static")
 
-app = Flask(__name__, template_folder=template_directory)
+app = Flask(__name__, template_folder=template_directory, static_folder=static_directory)
 
 register_routes(app)
 

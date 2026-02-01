b5e8c33700d939629f7e45a89edb1a9f788a5bdc
Andrew <adespinosa2002@gmail.com>
2026-02-01 15:39:27 -0800

    Completed rerouting for categories page

--- backend/routes/landing_routes/instrument.py
+++ backend/routes/landing_routes/instrument.py
@@ -7,32 +7,18 @@ def register_instrument(app):
     @app.route("/instrument_family/<family>/<instrument>")
 
     def instrument(family, instrument):
-
-        directory = os.path.join(current_app.root_path, 'content', family, instrument)
-
-        if not os.path.exists(directory):
-            abort(404)
-
-        categories = fill_categories(directory)
-
-        find_instrument_id = (db.table('instruments').select('id').eq("name", instrument).execute())
-        instrument_id = find_instrument_id.data[0]['id']
-        
         find_categories = db.table('categories').select('*, category_types(Name)').execute()
-        the_categories = find_categories.data[0]['category_types']['Name']
-        
-        
+        the_categories = find_categories.data
+        unique_categories = []
+
+        for i in the_categories:
+            c_type = i['category_types']['Name']
+            if c_type not in unique_categories:
+                unique_categories.append(c_type)
+
+        print(unique_categories)
 
         return render_template('landing_pages/instrument.html', 
                                family_name=family, 
                                instrument_name=instrument, 
-                               category_list=categories)
-    
-## Helper
-
-def fill_categories(directory):
-    categories = []
-    for category in os.listdir(directory):
-        categories.append(category)
-
-    return categories
+                               category_list=unique_categories)

ed523d76bd5747c0a461cd736c530c886d18be82
Andrew <adespinosa2002@gmail.com>
2026-02-01 16:11:23 -0800

    Finished categories page

--- backend/routes/landing_routes/categories.py
+++ backend/routes/landing_routes/categories.py
@@ -7,12 +7,14 @@ def register_categories(app):
     @app.route("/instrument_family/<family>/<instrument>/<category>")
 
     def category(family, instrument, category):
-
-        article_list = []
-        response = db.table('categories').select('*, articles(title)').execute()
+        response = db.table('categories').select('*, articles(title), category_types(Name)').eq('category_types.Name', f'{category}').execute()
         articles = response.data
 
-        for article in articles:
-            article_list.append(article['articles']['title'])
+        article_list = []
+
+        for i in articles:
+            if i['category_types'] is not None:
+                article_list.append(i['articles']['title'])
+                print("RAN")
 
         return render_template('landing_pages/categories.html', family_name=family, instrument_name=instrument, category_name=category, articles=article_list)

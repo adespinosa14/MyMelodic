67dd1f5e7d1598744c7488a2dfee48c758a8e97e
Andrew <adespinosa2002@gmail.com>
2026-02-06 11:53:27 -0800

    Disabled lacked content on home page

--- backend/routes/landing_routes/categories.py
+++ backend/routes/landing_routes/categories.py
@@ -7,13 +7,13 @@ def register_categories(app):
     @app.route("/instrument_family/<family>/<instrument>/<category>")
 
     def category(family, instrument, category):
-        response = db.table('categories').select('*, articles(title), instruments!inner(name), category_types!inner(Name)').eq('category_types.Name', category).eq('instruments.name', instrument).execute()
+        response = db.table('categories').select('*, articles(title, summary), instruments!inner(name), category_types!inner(Name)').eq('category_types.Name', category).eq('instruments.name', instrument).execute()
         articles = response.data
 
         article_list = []
 
         for i in articles:
             if i['category_types'] is not None:
-                article_list.append(i['articles']['title'])
+                article_list.append(i['articles'])
 
         return render_template('landing_pages/categories.html', family_name=family, instrument_name=instrument, category_name=category, articles=article_list)

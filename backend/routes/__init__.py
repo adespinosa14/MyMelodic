ed934232eaf059dd56ca323542e43ecdd8ee15c1
Andrew <adespinosa2002@gmail.com>
2026-02-07 14:49:41 -0800

    Added 'Contact Me' | Production Ready

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -4,6 +4,7 @@ from .landing_routes.instrument_family import register_instrument_family
 from .landing_routes.instrument import register_instrument
 from .landing_routes.categories import register_categories
 from .landing_routes.article import register_article
+from .landing_routes.contact_me import register_contact_me
 
 def register_routes(app):
     register_index(app)
@@ -11,3 +12,4 @@ def register_routes(app):
     register_instrument(app)
     register_categories(app)
     register_article(app)
+    register_contact_me(app)

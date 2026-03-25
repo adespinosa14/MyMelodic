5c00c879a3cb687b4c91f50428bce442ed2ce6e5
Andrew <adespinosa2002@gmail.com>
2026-03-24 22:57:18 -0700

    Fixed File Tree

--- backend/routes/auth_routes/login.py
+++ backend/routes/auth_routes/login.py
@@ -4,3 +4,9 @@ def register_login(app):
     @app.route("/login")
     def login():
         return render_template('landing_pages/login.html')
+
+    def login_request():
+        print("Login Requested")
+
+    def signup_requested():
+        print("Signup Requested")

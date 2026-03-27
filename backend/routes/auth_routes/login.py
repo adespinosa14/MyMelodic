b709bf613be7afb5380e260b9f0dfe3ed03bdf94
Andrew <adespinosa2002@gmail.com>
2026-03-27 15:01:33 -0700

    Completed Signup Process

--- backend/routes/auth_routes/login.py
+++ backend/routes/auth_routes/login.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, request, flash
+from backend.database.supabase import db
 
 def register_login(app):
     @app.route("/login")
@@ -11,8 +12,17 @@ def register_login(app):
             data = request.form
             email = data['email']
             password = data['password']
-            print(f'{email} -- {password}')
-            flash('This is an alart')
+
+            try:
+                db.sign_in_with_password({
+                    'email' : email,
+                    'password' : password
+                })
+
+                redirect(url_for(''))
+            except Exception as e:
+                flash(f'Login Error: {e}')
+                return redirect(url_for('login'))
 
         return redirect(url_for('login'))
 
@@ -26,7 +36,25 @@ def register_login(app):
             password = data['password']
             confirm_password = data['confirm_password']
 
-            print(f'{first_name} {last_name} \n{email}\n{password} - {confirm_password}')
-            
+            if password != confirm_password:
+                flash('Passwords Don\'t Match')
+                return redirect(url_for('login'))
+            try:
+                db.auth.sign_up({
+                    'email': email,
+                    'password': password,
+                    'options': {
+                        'data' : {
+                            'first_name': first_name,
+                            'last_name': last_name
+                        },
+                        'email_redirect_to': url_for('login', _external=True)
+                    }
+                })
 
+                flash('Signup Successful - Please Login', 'success')
+            except Exception as e:
+                flash(f'Signup Error: {e}')
+                return redirect(url_for('login'))
+            
         return redirect(url_for('login'))
